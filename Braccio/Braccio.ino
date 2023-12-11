38544af4c1cdb2d60232e869c6fc930482f40661
AlphaGaming7780 <69048433+AlphaGaming7780@users.noreply.github.com>
2023-12-11 13:25:37 +0100

    Update Braccio.ino

--- Braccio/Braccio.ino
+++ Braccio/Braccio.ino
@@ -15,13 +15,14 @@
 #include <Braccio.h>
 #include <Servo.h>
 
-
 Servo base;
 Servo shoulder;
 Servo elbow;
 Servo wrist_rot;
 Servo wrist_ver;
 Servo gripper;
+
+int m1 = 90, m2 = 90, m3 = 90, m4 = 90;
 
 void setup() {  
   //Initialization functions and set up the initial position for Braccio
@@ -32,22 +33,41 @@ void setup() {
   //Wrist vertical (M4): 180 degrees
   //Wrist rotation (M5): 90 degrees
   //gripper (M6): 10 degrees
+  Serial.begin(115200);
   Braccio.begin();
 }
 
 void loop() {
-  /*
-   Step Delay: a milliseconds delay between the movement of each servo.  Allowed values from 10 to 30 msec.
-   M1=base degrees. Allowed values from 0 to 180 degrees
-   M2=shoulder degrees. Allowed values from 15 to 165 degrees
-   M3=elbow degrees. Allowed values from 0 to 180 degrees
-   M4=wrist vertical degrees. Allowed values from 0 to 180 degrees
-   M5=wrist rotation degrees. Allowed values from 0 to 180 degrees
-   M6=gripper degrees. Allowed values from 10 to 73 degrees. 10: the toungue is open, 73: the gripper is closed.
-  */
-  
-  // the arm is aligned upwards  and the gripper is closed
-                     //(step delay, M1, M2, M3, M4, M5, M6);
-  Braccio.ServoMovement(20,         0, 90, 90, 90, 90,  73);  
+	/*
+	Step Delay: a milliseconds delay between the movement of each servo.  Allowed values from 10 to 30 msec.
+	M1=base degrees. Allowed values from 0 to 180 degrees
+	M2=shoulder degrees. Allowed values from 15 to 165 degrees
+	M3=elbow degrees. Allowed values from 0 to 180 degrees
+	M4=wrist vertical degrees. Allowed values from 0 to 180 degrees
+	M5=wrist rotation degrees. Allowed values from 0 to 180 degrees
+	M6=gripper degrees. Allowed values from 10 to 73 degrees. 10: the toungue is open, 73: the gripper is closed.
+	*/
+	
+	// the arm is aligned upwards  and the gripper is closed
+
+	// Serial.println(analogRead(A0));
+
+	// if(analogRead(A0) > 520) {
+	m1 += map(analogRead(A0), 0, 1023, -10, 10);
+	m2 += map(analogRead(A1), 0, 1023, -10, 10);
+	m3 += map(analogRead(A2), 0, 1023, -10, 10);
+	m4 += map(analogRead(A3)+2, 0, 1023, -10, 10);
+
+	// }
+	if(m1 > 180) m1 = 180;
+	if(m1 < 0) m1 = 0;
+	if(m2 > 180) m2 = 180;
+	if(m2 < 0) m2 = 0;
+	if(m3 > 180) m3 = 180;
+	if(m3 < 0) m3 = 0;
+	if(m4 > 180) m4 = 180;
+	if(m4 < 0) m4 = 0;
+					 	//(step delay, M1, M2, M3, M4, M5, M6);
+	Braccio.ServoMovement(10,         m1, m2, m3, m4, 90,  73);  
 	 
 }
