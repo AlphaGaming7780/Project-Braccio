9d419504789c17dbbf94a4f22deb24fae5362dfd
Math0XK <145477623+Math0XK@users.noreply.github.com>
2023-12-11 11:54:16 +0100

    Create Util.h

---
+++ Braccio/Util.h
@@ -0,0 +1,8 @@
+/*
+M1 : Moteur base = pin 11
+M2 : Moteur epaule = pin 10
+M3 : Moteur coude = pin 9
+M4 : Moteur poignet vertical = pin 6
+M5 : Moteur rotation poignet = pin 5 (déconnecté)
+M6 : Moteur pince = pin 3 (déconnecté)
+*/
